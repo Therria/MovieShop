66e7b9b9cfce772d28b7ac06e53b8cb760e2520d
Therria <therrialee@gmail.com>
2022-05-16 18:26:08 -0700

    Added moviedetailmodel

--- Infrastructure/Repositories/MovieRepository.cs
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
@@ -17,11 +18,24 @@ namespace Infrastructure.Repositories
 
         public List<Movie> GetTop30GrossingMovies()
         {
-            // Dapper
+
             // var top30Movies = movies.GetMovies().OrderByDescending(m => m.Revenue).Take(30);
             var movies = _dbContext.Movies.OrderByDescending(m => m.Revenue).Take(30).ToList();
 
             return movies;
+        }
+
+        public override Movie GetById(int id)
+        {
+            // Movie m LEFT JOIN MovieGenre mg on m.id = mg.movieid LEFT JOIN Genre g on mg.genreid = g.id  +
+            // Movie m LEFT JOIN MovieCast mc on m.id = mc.movieid LEFT JOIN Cast c on mc.castid = c.id +
+            // Movie m LEFT JOIN MovieCrew mc on m.id = mc.movieid LEFT JOIN Crew c on mc.crewid = c.id
+            var movie = _dbContext.Movies.Include(m => m.MoviesOfGenre).ThenInclude(m => m.Genre)
+                .Include(m => m.MoviesOfCast).ThenInclude(m => m.Cast)
+                .Include(m => m.MoviesOfCrew).ThenInclude(m => m.Crew)
+                .FirstOrDefault(m => m.Id == id);
+
+            return movie;
         }
     }
 }
