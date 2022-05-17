f99e6c1a67edbf4b4bbb93949cb89d85248c8dec
Therria <therrialee@gmail.com>
2022-05-16 17:47:42 -0700

    Added base repository

--- Infrastructure/Repositories/MovieRepository.cs
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationCore.Contracts.Repositories;
 using ApplicationCore.Entities;
+using Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,30 +9,17 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
 {
-    public class MovieRepository : IMovieRepository
+    public class MovieRepository : Repository<Movie>, IMovieRepository
     {
+        public MovieRepository(MovieShopDbContext dbContext) : base(dbContext)
+        {
+        }
+
         public List<Movie> GetTop30GrossingMovies()
         {
             // Dapper
             // var top30Movies = movies.GetMovies().OrderByDescending(m => m.Revenue).Take(30);
-            var movies = new List<Movie>()
-            {
-                new Movie {Id = 1, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 2, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 3, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 4, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 5, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 6, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 7, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 8, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 9, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 10, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 11, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 12, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 13, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 14, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
-                new Movie {Id = 15, Title ="Inception", PosterUrl ="https://image.tmdb.org/t/p/w342//9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"}
-            };
+            var movies = _dbContext.Movies.OrderByDescending(m => m.Revenue).Take(30).ToList();
 
             return movies;
         }
