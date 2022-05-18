8ac0948fcbbe3b4f5e6295edf3823680efb41efa
Therria <therrialee@gmail.com>
2022-05-18 00:48:29 -0700

    Added Pagenation to movieRepository

--- ApplicationCore/Contracts/Repositories/IMovieRepository.cs
+++ ApplicationCore/Contracts/Repositories/IMovieRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ApplicationCore.Entities;
+using ApplicationCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,10 @@ namespace ApplicationCore.Contracts.Repositories
     public interface IMovieRepository : IRepository<Movie>
     {
         Task<List<Movie>> GetTop30GrossingMovies();
+
+        // multiple return types : tuple
+        // Task<(List<Movie>, int totalCount, int totalPages)> GetMoviesByGenres(int genreId, int pageSize = 30, int pageNumber = 1);
+        
+        Task<PagedResultSet<Movie>> GetMoviesByGenres(int genreId, int pageSize = 30, int pageNumber = 1);
     }
 }
--- MovieShopMVC/Controllers/MoviesController.cs
+++ MovieShopMVC/Controllers/MoviesController.cs
@@ -17,5 +17,11 @@ namespace MovieShopMVC.Controllers
             var movie = await _movieService.GetMovieDetails(id);
             return View(movie);
         }
+
+        // .../Movies/Genres/1?
+        public async Task<IActionResult> Genres(int id, int pageSize = 30, int pageNumber = 1)
+        {
+            return View();
+        }
     }
 }
